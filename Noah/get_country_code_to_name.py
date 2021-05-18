6c00eaff30d1eb107ed53ac86f934283ae71eee8
Noah Schoem <nschoem@gmail.com>
2021-05-18 12:54:13 -0500

    Removed old country code/name mappings

--- Noah/get_country_code_to_name.py
+++
@@ -1,7 +0,0 @@
-"""
-This script reads the IBRD historical data to extract a mapping of country to country code.
-"""
-import pandas
-df=pandas.read_csv('IBRD_Statement_Of_Loans_-_Historical_Data.csv')
-country_name_code = df[['Country Code','Country']].drop_duplicates()
-country_name_code.to_csv('country_code_to_name.csv')
